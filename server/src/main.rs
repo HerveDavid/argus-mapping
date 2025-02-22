dc934617cd63390c872ac1bf8c6e6e65c58c5e13
HerveDavid <david.herve.code@gmail.com>
2025-02-22 19:47:43 +0100

    feat: Load a network.json file

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,6 @@
 use askama::Template;
 use axum::{
+    extract::Multipart,
     extract::State,
     response::Html,
     routing::{get, get_service, post},
@@ -7,7 +8,8 @@ use axum::{
 };
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::Arc;
-use tower_http::services::ServeDir;
+use tower_http::{services::ServeDir, trace::TraceLayer};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 // Définition du template
 #[derive(Template)]
@@ -21,6 +23,13 @@ struct IndexTemplate {
 #[template(path = "counter.html")]
 struct CounterTemplate {
     count: u64,
+}
+
+// Nouveau template pour le résultat JSON
+#[derive(Template)]
+#[template(path = "iidm_table.html")]
+struct IIdmTableTemplate {
+    iidm_table: String,
 }
 
 // État partagé de l'application
@@ -47,10 +56,37 @@ async fn increment(State(state): State<Arc<AppState>>) -> Html<String> {
     Html(template.render().unwrap())
 }
 
+// Handler pour l'upload
+async fn upload_iidm(mut multipart: Multipart) -> Html<String> {
+    let iidm_table = String::from("Aucun fichier reçu");
+
+    // while let Some(field) = multipart.next_field().await.unwrap() {
+    //     if field.name() == Some("iidm_file") {
+    //         if let Ok(bytes) = field.bytes().await {
+    //             tracing::info!("{:?}", bytes);
+    //             // if let Ok(json) = serde_json::from_slice::<Value>(&bytes) {
+    //             //     json_content = serde_json::to_string_pretty(&json)
+    //             //         .unwrap_or_else(|_| "Erreur de formatage JSON".to_string());
+    //             // } else {
+    //             //     json_content = "Fichier JSON invalide".to_string();
+    //             // }
+    //         }
+    //     }
+    // }
+
+    let template = IIdmTableTemplate { iidm_table };
+    Html(template.render().unwrap())
+}
+
 #[tokio::main]
 async fn main() {
     // Initialisation du logging
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     // État partagé
     let state = Arc::new(AppState {
@@ -61,7 +97,9 @@ async fn main() {
     let app = Router::new()
         .route("/", get(index))
         .route("/increment", post(increment))
+        .route("/upload", post(upload_iidm))
         .nest_service("/static", get_service(ServeDir::new("static")))
+        .layer(TraceLayer::new_for_http())
         .with_state(state);
 
     // Démarrage du serveur
