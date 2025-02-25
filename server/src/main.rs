e7fc1670b96259faf2eda2a80b89bcb6e13d4a14
HerveDav <david.herve@rte-france.com>
2025-02-25 16:24:09 +0100

    feat: Change api route

--- server/src/main.rs
+++ server/src/main.rs
@@ -27,8 +27,8 @@ async fn main() {
     // Build routes
     let app = Router::new()
         .route("/", get(index))
-        .route("/upload", post(upload_iidm))
-        .route("/update/{component_type}", post(update_iidm))
+        .route("/api/iidm/upload", post(upload_iidm))
+        .route("/api/iidm/update/{component_type}", post(update_iidm))
         .nest_service("/static", get_service(ServeDir::new(static_path)))
         .layer(TraceLayer::new_for_http())
         .with_state(Arc::new(AppState::default()));
